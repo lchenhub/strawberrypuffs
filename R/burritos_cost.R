d36d83db9dfb2633459a7fedaf3a4329f9daa667
lchenhub <liane330@gmail.com>
2023-08-18 14:28:31 -0700

    strawberry puffs

---
+++ R/burritos_cost.R
@@ -0,0 +1,14 @@
+#' Calculate total happiness from strawberry puffs
+#'
+#' @param sugar Amount of sugar consumed
+#' @param happy Utility points for resulting happiness
+#'
+#' @return returns the max happiness from strawberry puffs
+#' @export
+#'
+#' @examples
+
+
+strawberry_puffs <- function(sugar, happy) {
+  sugar * happy
+}
